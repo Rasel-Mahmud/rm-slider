2eb9c5ae65e96a8d295ed6456c025a87fba2276c
Rasel <rasel.wp@gmail.com>
2024-02-18 01:17:38 +0600

    Plugin init

---
+++ rm-slider.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+* Plugin Name:       RM Slider
+* Plugin URI:        https://rashel.pro/plugins/rm-slider/
+* Description:       Handle the basics with this plugin.
+* Version:           1.0.0
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Mahamud Hasan Rashel
+* Author URI:        https://rashel.pro/author
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI:        https://rashel.pro/rm-slider/
+* Text Domain:       rm-slider
+* Domain Path:       /languages
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
