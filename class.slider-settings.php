c4e837d2bf04360d51f3b696af3c138a105ceffd
Rasel <rasel.wp@gmail.com>
2024-02-24 00:15:15 +0600

    validate

--- class.slider-settings.php
+++ class.slider-settings.php
@@ -9,17 +9,17 @@ if( ! class_exists( 'RM_Slider_Settings' ) ) {
         }
 
         public function admin_init(){
-            register_setting( 'rm_slider_settings_group', 'rm_slider_settings' );
+            register_setting( 'rm_slider_settings_group', 'rm_slider_settings', [$this, 'rm_slider_validation']);
 
             add_settings_section( 'rm_slider_main', 'How does it works?', null, 'rm_slide_page1');
             add_settings_field( 'rm_slider_shortcode', 'Shortcode', [$this, 'rm_slider_shortcode_callback'], 'rm_slide_page1', 'rm_slider_main');
 
             add_settings_section( 'rm_slider_second', 'Other plugin options', null, 'rm_slide_page2');
-            add_settings_field( 'rm_slider_title', 'Title', [$this, 'rm_slider_title_callback'], 'rm_slide_page2', 'rm_slider_second');
+            add_settings_field( 'rm_slider_title', 'Title', [$this, 'rm_slider_title_callback'], 'rm_slide_page2', 'rm_slider_second', ['label_for' => 'rm_slider_title']);
 
-            add_settings_field( 'rm_slider_buttet', 'Display bullet', [$this, 'rm_slider_bullet_callback'], 'rm_slide_page2', 'rm_slider_second');
+            add_settings_field( 'rm_slider_buttet', 'Display bullet', [$this, 'rm_slider_bullet_callback'], 'rm_slide_page2', 'rm_slider_second', ['label_for' => 'rm_slider_bullet']);
 
-            add_settings_field( 'rm_slider_style', 'Choice Style', [$this, 'rm_slider_style_callback'], 'rm_slide_page2', 'rm_slider_second');
+            add_settings_field( 'rm_slider_style', 'Slider Style', [$this, 'rm_slider_style_callback'], 'rm_slide_page2', 'rm_slider_second', ['item' => ['style-1', 'style-2'], 'label_for' => 'rm_slider_style']);
 
         }
 
@@ -31,39 +31,51 @@ if( ! class_exists( 'RM_Slider_Settings' ) ) {
 
         public function rm_slider_title_callback(){
             ?>
-            <input type="text" name="rm_slider_settings[rm_slider_title]" value="<?php echo isset(self::$options) ? self::$options['rm_slider_title'] : ''; ?>">
+            <input type="text" name="rm_slider_settings[rm_slider_title]" id="rm_slider_title" value="<?php echo isset(self::$options['rm_slider_title']) ? self::$options['rm_slider_title'] : ''; ?>">
             <?php
         }
         
         public function rm_slider_bullet_callback(){
             ?>
-            <input type="checkbox" name="rm_slider_settings[rm_slider_bullet]" value="1"
+            <input type="checkbox" name="rm_slider_settings[rm_slider_bullet]" value="1" id="rm_slider_bullet"
             <?php
                 if( isset(self::$options['rm_slider_bullet']) ){
                     checked( "1", self::$options['rm_slider_bullet'], true );
                 }
             ?>>
+            <label for="rm_slider_bullet">whether to display bullet or not</label>
             <?php
         }
 
-        public function rm_slider_style_callback(){
-            ?>
-            <select name="rm_slider_settings[rm_slider_style]">
-                <option value="style-1"
-                    <?php echo isset(self::$options['rm_slider_style']) ? selected('style-1', self::$options['rm_slider_style'], true) : ''; ?>
-                > Style 1 </option>
-
-                <option value="style-2"
-                    <?php echo isset(self::$options['rm_slider_style']) ? selected('style-2', self::$options['rm_slider_style'], true) : ''; ?>
-                > Style 2 </option>
+        public function rm_slider_style_callback( $ags ){ ?>
+            <select name="rm_slider_settings[rm_slider_style]" id="rm_slider_style">
+            <?php foreach( $ags['item'] as $item) : ?>
+                <option value="<?php echo $item; ?>"
+                    <?php echo isset(self::$options['rm_slider_style']) ? selected($item, self::$options['rm_slider_style'], true) : ''; ?>
+                > <?php echo $item; ?> </option>
+                <?php endforeach; ?>
             </select>
             <?php
         }
+
+        public function rm_slider_validation( $input ){
+            $new_input = [];
+            foreach($input as $key => $value){
+
+                switch ($key) {
+                    case 'rm_slider_title' :
+                        $new_input[$key] = sanitize_text_field( $value );
+                        break;
+                    case 'rm_slider_bullet' :
+                        $new_input[$key] = absint( $value );
+                        break;
+                    default:
+                        $new_input[$key] = sanitize_text_field( $value );
+                        break;
+                }
+
+            }
+            return $new_input;
+        }
     }
 }
-
-// new RM_Slider_Settings();
-// echo "<pre>";
-// var_dump(RM_Slider_Settings::$options);
-// echo "</pre>";
-// die();
